fa3624dbf5f80ef268658aaf0a2992ab27858785
GUILHERME DE OLIVEIRA LIMA <22282086@CFP127ALUNO.LOCAL>
2023-04-14 11:28:38 -0300

    add

---
+++ aula-09/model/DAO/alunoDAO.js
@@ -0,0 +1,33 @@
+/********************************************************************
+ * Objetivo: Realizar a interação do Aluno com o Banco de Dados
+ * Data: 14/04/2023
+ * Autor: Guilherme Lima
+ * Versão: 1.0
+ *********************************************************************/
+
+// Inserir um novo registro no Banco de Dados
+const insertAluno = function (dadosAluno) {
+
+
+
+}
+
+// Atualizar um novo Registro no Banco de Dados
+const updateAluno = function (id) {
+
+}
+
+// Excluir um Registro do Banco de dados
+const deleteAluno = function (id) {
+
+}
+
+// Retorna todos os Registo do Banco de Dados
+const selectAllAluno = function () {
+
+}
+
+// Retorna um Registro filtrado pelo ID do Banco de Dados
+const selectByIdAluno = function (id) {
+
+}
